2eeb83789f84901b26ff72ed00902ecd00441151
victorcreativee <tuyizerevictor@gmail.com>
2024-11-17 15:31:49 -0800

    test

--- models/base_model.py
+++ models/base_model.py
@@ -17,18 +17,20 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialization of a BaseModel instance"""
-        if (len(kwargs) == 0):
+        if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
         else:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
             for key, val in kwargs.items():
-                if "__class__" not in key:
+                if key == "created_at" or key == "updated_at":
+                    # Try parsing with microseconds, fallback to without microseconds
+                    try:
+                        setattr(self, key, datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f"))
+                    except ValueError:
+                        setattr(self, key, datetime.strptime(val, "%Y-%m-%dT%H:%M:%S"))
+                elif key != "__class__":
                     setattr(self, key, val)
 
     def __str__(self):
